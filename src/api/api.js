d3a8b5d22a8532c2143329fdbfee2649afb12855
yosseferrazik <yosseferrazik@gmail.com>
2024-04-12 16:39:35 +0200

    Landing y Navbar

---
+++ src/api/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:4000";
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const get = async (endpoint) => {
+  try {
+    const response = await api.get(endpoint);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const post = async (endpoint, data) => {
+  try {
+    const response = await api.post(endpoint, data);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
